de208ea8818b1597b43c153de4cdbebd35efee1c
Walter Larrea <walterlarrea@live.com>
2023-12-27 22:43:26 -0300

    Initial commit:

---
+++ app.js
@@ -0,0 +1,20 @@
+import express from 'express'
+import cors from 'cors'
+import usersRouter from './controllers/users.js'
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (request, response) => {
+    response.send('<h1>Hello World!</h1>')
+})
+
+app.use('/api/user', usersRouter)
+
+app.get('/api/login', (request, response) => {
+    response.json({ LOGIN: 'endpoint' })
+})
+
+export default app
---
+++ index.js
@@ -0,0 +1,9 @@
+import app from './app.js'
+import http from 'http'
+import config from './utils/config.js'
+
+const server = http.createServer(app)
+
+server.listen(config.SERVER_PORT, () => {
+  console.log(`Server running on port ${config.SERVER_PORT}`)
+})
