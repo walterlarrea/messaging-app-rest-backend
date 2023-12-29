8a1a575ff8087a416c35302a06ddff668bc1c10d
Walter Larrea <walterlarrea@live.com>
2023-12-29 20:50:03 -0300

    add: mysql connection for new session
    This function allows for easy new sessions without need to specify a table

--- src/utils/mySqlConnection.js
+++ src/utils/mySqlConnection.js
@@ -1,6 +1,13 @@
 import mysqlx from '@mysql/xdevapi'
 import mysqlConfig from '../constants/mysqlCredentials.js'
 
+export const getSession = async function () {
+	const mySession = await mysqlx.getSession(mysqlConfig)
+	const myDb = mySession.getSchema(mysqlConfig.schema)
+
+	return [myDb, mySession.close]
+}
+
 const getSessionForTable = async function (table) {
 	const mySession = await mysqlx.getSession(mysqlConfig)
 	const myDb = mySession.getSchema(mysqlConfig.schema)
