3e734b24b879ef727734d07421810069013f8579
Walter Larrea <walterlarrea@live.com>
2024-01-06 02:33:25 -0300

    test: dockerfile and mutual volume/network

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import app from './app.js'
 import http from 'http'
-import { SERVER_PORT, } from './src/constants/config.js'
+import { SERVER_PORT, MYSQL_PORT } from './src/constants/config.js'
 
 const server = http.createServer(app)
 
 server.listen(SERVER_PORT, () => {
-	console.log(`Server running on port ${SERVER_PORT}`)
+	console.log(
+		`Server running on port ${SERVER_PORT} and mySQL port ${MYSQL_PORT}`
+	)
 })
