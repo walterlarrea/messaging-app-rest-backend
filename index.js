495fd9591d550919b270cea970c735d3c996f461
Walter Larrea <walterlarrea@live.com>
2024-01-06 03:13:50 -0300

    add: working interconnecting containers

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 import app from './app.js'
 import http from 'http'
-import { SERVER_PORT, MYSQL_PORT } from './src/constants/config.js'
+import { SERVER_PORT } from './src/constants/config.js'
 
 const server = http.createServer(app)
 
 server.listen(SERVER_PORT, () => {
-	console.log(
-		`Server running on port ${SERVER_PORT} and mySQL port ${MYSQL_PORT}`
-	)
+	console.log(`Server running on port ${SERVER_PORT}`)
 })
