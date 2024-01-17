d1f015a9c7018fb72d6059543e2c00c9502eae38
Walter Larrea <walterlarrea@live.com>
2024-01-17 12:53:30 -0300

    add: mongo db connection

--- src/constants/config.js
+++ src/constants/config.js
@@ -7,8 +7,4 @@ export const MYSQL_USER = process.env.MYSQL_USER
 export const MYSQL_PASSWORD = process.env.MYSQL_PASSWORD
 export const MYSQL_DATABASE = process.env.MYSQL_DATABASE
 
-export const MONGODB_URI = `mongodb://${process.env.MONGO_ROOT_USERNAME}
-:${process.env.MONGO_ROOT_PASSWORD}
-@${process.env.MONGO_HOST}:${process.env.MONGO_PORT}
-/${process.env.MONGO_DATABASE}
-?retryWrites=true&w=majority`
+export const MONGODB_URI = `mongodb://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.MONGO_DATABASE}`
