f20685a59789a6c8e8ad49c7dc745af4482b63a8
Walter Larrea <walterlarrea@live.com>
2024-01-18 14:02:55 -0300

    fix: users are active by default

--- src/constants/allowedOrigins.js
+++ src/constants/allowedOrigins.js
@@ -1,7 +1,10 @@
 const allowedOrigins = [
-	'http://localhost:4321',
-	'http://localhost:4322',
-	'http://localhost:3001',
+	'http://localhost:3002', // Test Node
+	'http://localhost:4322', // Test Astro ?
+	'http://localhost:3001', // Development Node
+	'http://localhost:4321', // Development Astro
+	'http://localhost:8080', // Production Node local
+	'http://localhost:5000', // Production Node docker container
 ]
 
 export default allowedOrigins
