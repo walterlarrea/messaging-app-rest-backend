2333331a90294aab518a8bd3922339208201f63d
Walter Larrea <walterlarrea@live.com>
2024-02-22 15:37:07 -0300

    add: allowed origins for cors configuration

--- src/constants/allowedOrigins.js
+++ src/constants/allowedOrigins.js
@@ -5,6 +5,10 @@ const allowedOrigins = [
 	'http://localhost:4321', // Development Astro
 	'http://localhost:8080', // Production Node local
 	'http://localhost:5000', // Production Node docker container
+	'http://localhost:80', // Production Node docker container
+	'http://localhost', // Production Node docker container
+	// 'http://messagingapp.epd7hce8bbb7dsd0.brazilsouth.azurecontainer.io', // Production
+	'https://messaging-app.azurewebsites.net', // Production
 ]
 
 export default allowedOrigins
